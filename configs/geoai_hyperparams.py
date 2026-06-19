1fc4678a3399f066a1af25308087171a204cc689
ngTanPhuc <nguyentanphuc020505@gmail.com>
2026-06-19 14:45:35 +0700

    Change lr to 0.005

--- configs/geoai_hyperparams.py
+++ configs/geoai_hyperparams.py
@@ -2,7 +2,7 @@ hyperparams = {
     "num_channels": 4,
     "num_classes": 2,
     "batch_size": 4,
-    "learning_rate": 0.0001,
+    "learning_rate": 0.005,
     "tile_size": 512,
     "stride": 256,
     "val_split": 0.2,
